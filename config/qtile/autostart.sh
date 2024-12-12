d1c0266755b4f31b9e828884a7ee5b9fb12964f2
cybardev <sheikh@cybar.dev>
2024-12-12 01:45:10 -0400

    feat: update to match current EndeavourOS config

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-feh --bg-scale ~/Pictures/Wallpapers/minimalist-mountains-green.jpg
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
-xfce4-panel & disown
+feh --no-fehbg --bg-center "/home/sage/Pictures/wallpapers/EOS-PLANETS-ARTEMIS.png" # /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+picom & disown # --experimental-backends --vsync # should prevent screen tearing on most setups if needed
 
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
@@ -10,4 +8,4 @@ xfce4-panel & disown
 # Start welcome
 # eos-welcome & disown
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
--- config/qtile/scripts/check_battery.sh
+++ config/qtile/scripts/check_battery.sh
@@ -9,7 +9,7 @@ low_bat=26
 #Do not modify these variables
 charging="fully-charged"
 not_charging="discharging"
-bat_now=$(cat /sys/class/power_supply/BAT1/capacity)
+bat_now=$(cat /sys/class/power_supply/BAT0/capacity)
 check=2
 
 #I check if the battery is not charging, the script is not running and the battery
@@ -20,13 +20,13 @@ check=2
 while true
 do
 	check_running=$(pgrep -fl low_battery.sh)
-	state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep state | awk '{print $2}')
+	state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}')
 	if [ "$state" = "$charged" ]; then
 		check=1
 		sleep 30
 	elif [ "$state" = "$not_charging" ] && [ -z "$check_running" ] && ( [ "$bat_now" -gt "$low_bat" ]  || [ "$check" -lt 2 ] );then
-		~/.config/qtile/scripts/low_bat.sh
 	else
 		sleep 30
+		~/.config/qtile/scripts/low_bat_notifier.sh
 	fi
 done
